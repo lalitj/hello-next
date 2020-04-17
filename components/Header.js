62360f657f78193ea57ad53dfc7bd3a9107ff537
Lalit J <lalit@extreme-agile.com>
2020-04-17 16:15:32 +0530

    7. Styling Components.

--- components/Header.js
+++ components/Header.js
@@ -13,6 +13,9 @@ export default function Header() {
       <Link href="/about">
         <a style={linkStyle}>About</a>
       </Link>
+      <Link href="/markdown">
+        <a style={linkStyle}>Markdown</a>
+      </Link>
     </div>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -2,18 +2,49 @@ import Layout from '../components/MyLayout';
 import Link from 'next/link';
 import fetch from 'isomorphic-unfetch';
 
+const PostLink = ({ post }) => (
+  <li>
+    <Link href="/p/[id]" as={`/p/${post.id}`}>
+      <a>{post.name}</a>
+    </Link>
+    <style jsx>{`
+      li {
+        list-style: none;
+        margin: 5px 0;
+      }
+
+      a {
+        text-decoration: none;
+        color: blue;
+        font-family: 'Arial';
+      }
+
+      a:hover {
+        opacity: 0.6;
+      }
+    `}</style>
+  </li>
+);
+
 const Index = props => (
   <Layout>
     <h1>Batman TV Shows</h1>
     <ul>
       {props.shows.map(show => (
-        <li key={show.id}>
-          <Link href="/p/[id]" as={`/p/${show.id}`}>
-            <a>{show.name}</a>
-          </Link>
-        </li>
+        <PostLink key={show.id} post={show} />
       ))}
     </ul>
+    <style jsx>{`
+        h1,
+        a {
+          font-family: 'Arial';
+        }
+
+        ul {
+          padding: 0;
+        }
+        `}
+      </style>
   </Layout>
 );
 
